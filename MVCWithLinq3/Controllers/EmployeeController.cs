011a8ff6b4da24c4931381bfac4ee255530ce53c
psagar kumar <psagarkmr@gmail.com>
2022-06-13 12:52:12 +0530

    Add project files.

---
+++ MVCWithLinq3/Controllers/EmployeeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCWithLinq3.Models;
+
+namespace MVCWithLinq3.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        EmployeeDAL obj = new EmployeeDAL();
+        public ViewResult DisplayEmployees()
+        {
+            return View(obj.GetEmployees());
+        }
+        public ViewResult DisplayEmployee(int Eid)
+        {
+            return View(obj.GetEmployee(Eid));
+        }
+        [HttpGet]
+        public ViewResult AddEmployee()
+        {
+            EmpDept emp = new EmpDept();
+            emp.Departments = obj.GetDeprtments();
+            return View(emp);
+        }
+        [HttpPost]
+        public RedirectToRouteResult AddEmployee(EmpDept emp)
+        {
+            obj.Employee_Insert(emp);
+            return RedirectToAction("DisplayEmployees");
+        }
+        public ViewResult EditEmployee(int Eid)
+        {
+            EmpDept Emp= obj.GetEmployee(Eid);
+            Emp.Departments = obj.GetDeprtments();
+            return View(Emp);
+        }
+        public RedirectToRouteResult UpdateEmployee(EmpDept emp)
+        {
+            obj.Employee_Update(emp);
+            return RedirectToAction("DisplayEmployees");
+        }
+        public RedirectToRouteResult DeleteEmployee(int Eid)
+        {
+            obj.Employee_Delete(Eid);
+            return RedirectToAction("DisplayEmployees");
+        }
+   }
+}
---
+++ MVCWithLinq3/Models/EmployeeDAL.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCWithLinq3.Models
+{
+    public class EmployeeDAL
+    {
+        MVCDBDataContext dc = new MVCDBDataContext();
+
+        // public object Item { get; private set; }
+
+        public List<SelectListItem> GetDeprtments()
+        {
+            List<SelectListItem> Depts = new List<SelectListItem>();
+            foreach (var item in dc.Departments)
+            {
+                SelectListItem li = new SelectListItem { Text = item.Dname, Value = item.Did.ToString() };
+                Depts.Add(li);
+            }
+            return Depts;
+        }
+        public EmpDept GetEmployee(int Eid)
+        {
+            var Record = (from E in dc.Employees join D in dc.Departments on E.Did equals D.Did where E.Eid == Eid select new { E.Eid, E.Ename, E.job, E.salary, D.Did, D.Dname, D.Location }).Single();
+            EmpDept obj = new EmpDept { Eid = Record.Eid, Ename = Record.Ename, job = Record.job, salary = Record.salary, Did = Record.Did, Dname = Record.Dname, Location = Record.Location };
+            return obj;
+        }
+        public List<EmpDept> GetEmployees()
+        {
+            var Records = (from E in dc.Employees join D in dc.Departments on E.Did equals D.Did where E.Status == true select new { E.Eid, E.Ename, E.job, E.salary, D.Did, D.Dname, D.Location });
+            List<EmpDept> Emps = new List<EmpDept>();
+            foreach (var Record in Records)
+            {
+                EmpDept obj = new EmpDept { Eid = Record.Eid, Ename = Record.Ename, job = Record.job, salary = Record.salary, Did = Record.Did, Dname = Record.Dname, Location = Record.Location };
+                Emps.Add(obj);
+            }
+            return Emps;
+        }
+        public void Employee_Insert(EmpDept obj)
+        {
+            Employee Emp = new Employee { Ename = obj.Ename, job = obj.job, salary = obj.salary, Did = obj.Did, Status = true };
+            dc.Employees.InsertOnSubmit(Emp);
+            dc.SubmitChanges();
+        }
+        public void Employee_Update(EmpDept NewValues)
+        {
+            Employee OldValues = dc.Employees.Single(E => E.Eid == NewValues.Eid);
+            OldValues.Ename = NewValues.Ename;
+            OldValues.job = NewValues.job;
+            OldValues.salary = NewValues.salary;
+            OldValues.Did = NewValues.Did;
+            dc.SubmitChanges();
+        }
+        public void Employee_Delete(int Eid)
+        {
+            Employee OldValues = dc.Employees.Single(E => E.Eid == Eid);
+            OldValues.Status = false;
+            dc.SubmitChanges();
+        }
+    }
+}
